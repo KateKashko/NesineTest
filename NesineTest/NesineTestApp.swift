45c8d735b209273a7f541ecdbab43ffd0ac02dc9
KateKashko <107617602+KateKashko@users.noreply.github.com>
2023-10-13 21:50:45 +0300

    Initial Commit

---
+++ NesineTest/NesineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NesineTestApp.swift
+//  NesineTest
+//
+//  Created by Kate Kashko on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NesineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
